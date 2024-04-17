3057045c14cb1cee9b0c982d46884b3f0848e0d4
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-17 04:23:00 -0400

    init

--- App.js
+++ App.js
@@ -3,11 +3,13 @@
     import express from 'express';
     import cors from "cors";
     import CourseRoutes from "./Kanbas/courses/routes.js";
+    import ModuleRoutes from "./Kanbas/modules/routes.js";
     const app = express();
     
     app.use(cors());
     app.use(express.json());
     CourseRoutes(app);
+    ModuleRoutes(app);
     Lab5(app);
     Hello(app);
 
