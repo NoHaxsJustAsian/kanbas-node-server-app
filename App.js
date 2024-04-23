e9f6572704cff5e9dad7b3925d7e38e0e207de60
Win Tongtawee <WinTongtawee@gmail.com>
2024-04-23 01:32:45 -0400

    a6

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
     import Hello from "./Hello.js";
     import Lab5 from "./Lab5.js";
+    import "dotenv/config";
     import express from 'express';
     import cors from "cors";
     import CourseRoutes from "./Kanbas/courses/routes.js";
     import ModuleRoutes from "./Kanbas/modules/routes.js";
+    import mongoose from 'mongoose';
+    import UserRoutes from "./Users/routes.js";
+    import session from "express-session";
     const app = express();
+    const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
     
-    app.use(cors());
+
+
+    app.use(
+        cors({
+          credentials: true,
+          origin: process.env.FRONTEND_URL
+        })
+       );
+       const sessionOptions = {
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+      };
+      if (process.env.NODE_ENV !== "development") {
+        sessionOptions.proxy = true;
+        sessionOptions.cookie = {
+          sameSite: "none",
+          secure: true,
+          domain: process.env.HTTP_SERVER_DOMAIN,
+        };
+      }
+      app.use(session(sessionOptions));
+      
+      
+    mongoose.connect(CONNECTION_STRING);
     app.use(express.json());
+    UserRoutes(app);
     CourseRoutes(app);
     ModuleRoutes(app);
     Lab5(app);
