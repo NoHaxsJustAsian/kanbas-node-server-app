3d8f86e0e9cf9066a72c26bc990c89103b69523a
Win Tongtawee <WinTongtawee@gmail.com>
2024-04-23 02:11:31 -0400

    Update App.js

--- App.js
+++ App.js
@@ -12,18 +12,17 @@
     const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
     
 
-
+    const sessionOptions = {
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    };
     app.use(
         cors({
           credentials: true,
           origin: [process.env.FRONTEND_URL, "https://a6--tunwa-tongtawee.netlify.app", "https://tunwa-tongtawee.netlify.app"]
         })
        );
-       const sessionOptions = {
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-      };
       if (process.env.NODE_ENV !== "development") {
         sessionOptions.proxy = true;
         sessionOptions.cookie = {
